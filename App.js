02714f0c647c2de3afff8052258d5b4a53f74f94
simon pineda <simonpineda0521@gmail.com>
2023-09-15 22:16:54 -0500

    the backgroud is finished

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Image, SafeAreaView, Button } from 'react-native';
+import { useState } from 'react';
+import { useFonts } from 'expo-font';
+import logo from './assets/Colibri.png'
+import bg from './assets/bg.png'
 
 export default function App() {
+  const [text, onChangeText] = useState('');
+  const [number, onChangeNumber] = useState('');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! Sisas</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <Image style={styles.bg} blurRadius={170} source={bg} />
+      <SafeAreaView>
+        <Image style={styles.logo} source={logo} />
+        <Text style={styles.title}>Welcome</Text>
+        <View>
+          <Text style={styles.inputDescrip}>Username:</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="SimonPine"
+            onChangeText={onChangeText}
+            value={text}
+          />
+          <Text style={styles.inputDescrip}>Pasword:</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={onChangeNumber}
+            value={number}
+            placeholder="********"
+          />
+        </View>
+      </SafeAreaView>
     </View>
   );
 }
@@ -13,8 +40,34 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#102026',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    borderColor: '#ffffff',
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    width: 'auto'
+  },
+  logo: {
+    width: 200,
+    height: 200,
+    // zIndex: 1000
+  },
+  bg: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%'
+  },
+  inputDescrip: {
+    color:'#fff',
+    fontSize: 20,
+  },
+  title: {
+    color: '#fff',
+    fontSize: 25,
+    },
 });
---
+++ api.js
@@ -0,0 +1,181 @@
+// export const urlBase = 'http://localhost:2000'
+export const urlBase = 'https://clubhub-backend.up.railway.app'
+
+//===========================Users
+
+const API = urlBase + '/user'
+const AP2 = urlBase + '/users'
+const API3 = urlBase + '/club'
+export const usersImg = urlBase + '/images/usersImg/'
+
+//==========================Clubs
+
+const APIc = urlBase + '/clubs'
+const API3e =  urlBase + '/userExitClub'
+export const BannersImg = urlBase + '/images/banners/'
+export const chatsFlies = urlBase + '/images/chats/'
+export const surveysBanner = urlBase + '/images/surveys/'
+
+
+export const getUser = async (id) => {
+    const res = await fetch(`${API}/${id}`)
+    return await res.json()
+}
+
+export const getUserName = async (id) => {
+    const res = await fetch(`${API}Name/${id}`)
+    return await res.json()
+}
+
+export const putUser = async (userName, pasword, question, answer) => {
+    const obj = await {
+        userName,
+        clubs: [],
+        pasword,
+        question,
+        answer,
+        description: "👋 Hey there! I'm using ClubHub"
+    }
+    const res = await fetch(AP2, {
+        method: 'POST',
+        headers: { Accept: 'application/json', "Content-Type": 'application/json' },
+        body: JSON.stringify(obj)
+    })
+    return await res.json()
+}
+
+export const editUser = async (userName, clubs, pasword, userImg, question, answer, description, oldUserName) => {
+    const obj = await {
+        userName,
+        clubs,
+        pasword,
+        userImg,
+        question,
+        answer,
+        description,
+    }
+    await fetch(`${API}/${oldUserName}`, {
+        method: 'PUT',
+        headers: { Accept: 'application/json', "Content-Type": 'application/json' },
+        body: JSON.stringify(obj)
+    })
+
+}
+
+
+export const uploadFile = async (body) => {
+
+    await fetch(`${API}/photo/upload`, {
+        method: 'POST',
+        body: body
+    })
+
+}
+
+export const newClub = async (body) => {
+
+    await fetch(`${APIc}/upload`, {
+        method: 'POST',
+        body: body
+    })
+
+}
+
+export const getClubs = async () => {
+    const res = await fetch(`${APIc}`)
+    return await res.json()
+}
+
+export const getClubId = async (id) => {
+    const res = await fetch(`${API3}/${id}`)
+    return await res.json()
+}
+
+export const joinToClub = async (obj) => {
+    await fetch(`${APIc}/join`, {
+        method: 'PUT',
+        headers: { Accept: 'application/json', "Content-Type": 'application/json' },
+        body: JSON.stringify(obj)
+    })
+}
+
+export const exitClub = async (obj) => {
+    await fetch(`${API3e}`, {
+        method: 'PUT',
+        headers: { Accept: 'application/json', "Content-Type": 'application/json' },
+        body: JSON.stringify(obj)
+    })
+}
+
+export const deleteClub = async (obj, id) => {
+    await fetch(`${APIc}/${id}`, {
+        method: 'DELETE',
+        headers: { Accept: 'application/json', "Content-Type": 'application/json' },
+        body: JSON.stringify(obj)
+    })
+}
+export const editClub = async (obj, id) => {
+    await fetch(`${API3}/${id}`, {
+        method: 'PUT',
+        body: obj
+    })
+}
+export const saveGrades = async (obj) => {
+    await fetch(`${API3}/grades`, {
+        method: 'POST',
+        headers: { Accept: 'application/json', "Content-Type": 'application/json' },
+        body: JSON.stringify(obj)
+    })
+
+}
+
+export const sendMsgEvent = async (obj) => {
+    await fetch(`${API3}/events`, {
+        method: 'POST',
+        body: obj
+    })
+
+}
+
+export const sendMsgChat = async (obj) => {
+    await fetch(`${API3}/chat`, {
+        method: 'POST',
+        body: obj
+    })
+
+}
+
+export const calendarUpdate = async (obj) => {
+    await fetch(`${API3}/calendar`, {
+        method: 'POST',
+        headers: { Accept: 'application/json', "Content-Type": 'application/json' },
+        body: obj
+    })
+
+}
+
+export const addSurveyToServer = async (obj) => {
+    await fetch(`${API3}/addSurvey`, {
+        method: 'POST',
+        body: obj
+    })
+
+}
+
+export const addRes = async (obj) => {
+    await fetch(`${API3}/addRes`, {
+        method: 'POST',
+        headers: { Accept: 'application/json', "Content-Type": 'application/json' },
+        body: obj
+    })
+
+}
+
+export const deleteSurvey = async (obj) => {
+    await fetch(`${API3}/deleteSurvey`, {
+        method: 'DELETE',
+        headers: { Accept: 'application/json', "Content-Type": 'application/json' },
+        body: obj
+    })
+
+}
